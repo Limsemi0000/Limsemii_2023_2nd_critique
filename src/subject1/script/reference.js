7d81b392f2294daad9ab076da106fac8366eea21
Limsemi <139162516+Limsemi@users.noreply.github.com>
2023-09-30 08:00:55 +0900

    first commit

---
+++ src/subject1/script/reference.js
@@ -0,0 +1,37 @@
+const reference = [
+  {
+    author: "앙투안 드 생텍쥐페리 원저/심재홍 번역 | 이팝출판사",
+    title: "에린 왕자 전라북도(Jeollabuk-do Dialect)",
+    year: "2021",
+    sourceTitle: "",
+    link: "",
+  },
+  {
+    author: "앙투안 드 생텍쥐페리 원저/최현애 번역 | 이팝출판사",
+    title: "애린 왕자 갱상도 (Gyeongsang-do Dialect)",
+    year: "2021",
+    sourceTitle: "",
+    link: "",
+  },
+  {
+    author: "freepik",
+    title: "커피와 야생화와 책 구성",
+    year: "",
+    sourceTitle: "",
+    link: "출처 <a href="https://kr.freepik.com/free-psd/book-composition-with-coffee-and-wildflowers_1358963.htm#page=2&query=%EC%B1%85&position=37&from_view=search&track=sph">Freepik</a>",
+  },
+  {
+    author: "@rawpixel.com",
+    title: "빈 책 또는 잡지 템플릿 이랑",
+    year: "",
+    sourceTitle: "",
+    link: "<a href="https://kr.freepik.com/free-psd/blank-book-or-magazine-template-mockup_3384952.htm#query=book&position=2&from_view=search&track=sph">작가 rawpixel.com</a> 출처 Freepik",
+  },
+  {
+    author: "High quality design tools",
+    title: "ree Open Magazine PSD Mockup",
+    year: "",
+    sourceTitle: "",
+    link: "https://www.ls.graphics/free/free-open-magazine-psd-mockup",
+  },
+];
